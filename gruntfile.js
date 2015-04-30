35ccbad6ca9c167b10732e7fcb2b2e949cbd610d
Jack Palfrey <jack.palf@gmail.com>
2015-04-30 18:53:49 +0100

    cleaner gruntfile

--- gruntfile.js
+++ gruntfile.js
@@ -1,13 +1,12 @@
 module.exports = function (grunt) {
     'use strict';
-
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json')
     });
-
     grunt.loadTasks('tasks');
-
     grunt.registerTask('default', [
-        'lint', 'test', 'package'
+        'lint',
+        'test',
+        'package'
     ]);
 };
